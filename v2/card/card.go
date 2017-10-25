e9d0225ff538c22e541933d975aa534c38b90aee
Christopher Waldon <christopher.waldon.dev@gmail.com>
2017-10-25 10:46:42 -0400

    First constructor passing tests

--- v2/card/card.go
+++ v2/card/card.go
@@ -1,7 +1,7 @@
 package card
 
 import (
-	//"fmt"
+	"fmt"
 	//"github.com/pkg/errors"
 	"github.com/sorribas/shamir3pass"
 	. "github.com/whereswaldon/cryptage/v2/types"
@@ -14,7 +14,12 @@ import (
 // will results in errors because the card has not been
 // encrypted by another party.
 func NewCard(face string, myKey *shamir3pass.Key) (Card, error) {
-	return nil, nil
+	if face == "" {
+		return nil, fmt.Errorf("Unable to create card with empty string as face")
+	} else if myKey == nil {
+		return nil, fmt.Errorf("Unable to create card with nil key pointer")
+	}
+	return &card{}, nil
 }
 
 // CardFrom creates a card from the given big integer. This
@@ -28,5 +33,24 @@ func CardFromTheirs(theirs *big.Int, myKey *shamir3pass.Key) (Card, error) {
 // assumes that the provided integer is the encrypted value
 // after both players have encrypted the card.
 func CardFromBoth(both *big.Int, myKey *shamir3pass.Key) (Card, error) {
+	return nil, nil
+}
+
+type card struct {
+}
+
+// ensure that the card type always satisfies the Card interface
+var _ Card = &card{}
+
+func (c *card) Face() (string, error) {
+	return "", nil
+}
+func (c *card) Mine() (*big.Int, error) {
+	return nil, nil
+}
+func (c *card) Theirs() (*big.Int, error) {
+	return nil, nil
+}
+func (c *card) Both() (*big.Int, error) {
 	return nil, nil
 }
