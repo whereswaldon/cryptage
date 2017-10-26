6c62a2e961b160b090bf951d14fccc4f2cb885fa
Christopher Waldon <christopher.waldon.dev@gmail.com>
2017-10-25 22:58:38 -0400

    Implement tests for adding both encrypted cards to cardholders

--- v2/card_holder/card_holder_test.go
+++ v2/card_holder/card_holder_test.go
@@ -82,4 +82,46 @@ var _ = Describe("CardHolder", func() {
 			})
 		})
 	})
+	Describe("Setting the both encrypted values for all cards", func() {
+		Context("When the input slice is empty", func() {
+			It("Should return an error", func() {
+				key := shamir3pass.GenerateKey(1024)
+				holder, err := NewHolder(&key, Faces)
+				Expect(err).To(BeNil())
+				Expect(holder).ToNot(BeNil())
+				err = holder.SetBothEncrypted([]*big.Int{})
+				Expect(err).ToNot(BeNil())
+			})
+		})
+		Context("When the input slice is nil", func() {
+			It("Should return an error", func() {
+				key := shamir3pass.GenerateKey(1024)
+				holder, err := NewHolder(&key, Faces)
+				Expect(err).To(BeNil())
+				Expect(holder).ToNot(BeNil())
+				err = holder.SetBothEncrypted(nil)
+				Expect(err).ToNot(BeNil())
+			})
+		})
+		Context("When the input slice has a different number of values", func() {
+			It("Should return an error", func() {
+				key := shamir3pass.GenerateKey(1024)
+				holder, err := NewHolder(&key, Faces)
+				Expect(err).To(BeNil())
+				Expect(holder).ToNot(BeNil())
+				err = holder.SetBothEncrypted([]*big.Int{big.NewInt(0)})
+				Expect(err).ToNot(BeNil())
+			})
+		})
+		Context("When the arguments are valid", func() {
+			It("Should not error", func() {
+				key := shamir3pass.GenerateKey(1024)
+				holder, err := NewHolder(&key, Faces)
+				Expect(err).To(BeNil())
+				Expect(holder).ToNot(BeNil())
+				err = holder.SetBothEncrypted([]*big.Int{})
+				Expect(err).To(BeNil())
+			})
+		})
+	})
 })
