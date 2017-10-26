28af8ad9d6f852119d4dbb17a215dc671b9700a9
Christopher Waldon <christopher.waldon.dev@gmail.com>
2017-10-26 14:16:18 -0400

    Fix deck package imports

--- v2/deck/deck.go
+++ v2/deck/deck.go
@@ -1,12 +1,13 @@
 package deck
 
 import (
+	"github.com/whereswaldon/cryptage/v2/card"
 	"math/big"
 )
 
 // CardHolder is an ordered collection of cards.
 type CardHolder interface {
-	Get(uint) (CardFace, error)
+	Get(uint) (card.CardFace, error)
 	SetBothEncrypted([]*big.Int) error
 	GetAllMine() ([]*big.Int, bool, error)
 }
